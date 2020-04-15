6c456fc3a103dedf1adaaf47263448771d408bb0
rafaelitajahy <rafaelitajahy@gmail.com>
2020-04-15 11:42:48 -0300

    Configuring EditorConfig

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express, { request } from 'express';
+import express, { request } from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
 });
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+  console.log("Server started on port 3333!");
 });
